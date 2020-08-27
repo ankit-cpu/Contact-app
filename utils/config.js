520453b6e8e3728006553b3dfce88f774d290256
ankit-cpu <62169774+ankit-cpu@users.noreply.github.com>
2020-08-27 11:47:43 +0530

    Add files via upload

---
+++ utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyCWyjJ-TCLK4-JnrKlWMdeQa0K6YgbkaiQ",
+  authDomain: "mygithub-95958.firebaseapp.com",
+  databaseURL: "https://mygithub-95958.firebaseio.com",
+  projectId: "mygithub-95958",
+  storageBucket: "mygithub-95958.appspot.com",
+  messagingSenderId: "493607303435",
+  appId: "1:493607303435:web:45c6bddfcc8373af140a0e",
+  measurementId: "G-QCEJF0KR1E"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
